3720f2766b2b52b42b6a0d44d35ee28cbfbedfe2
DeepikaaAPA <deepikaarjun@gmail.com>
2024-07-05 15:03:34 +0530

    css changes

--- src/BlogCard.jsx
+++ src/BlogCard.jsx
@@ -3,10 +3,12 @@ export function BlogCard({ blog }) {
     <>
       <div className="blog-card">
         <img src={blog.thumbnail}></img>
+        <h3 className="blog-title">{blog.title}</h3>
         <div className="card-desc">
-          <h3 className="blog-title">{blog.title}</h3>
+          
           <p>By {blog.author}</p>
           <div className="card-footer">
+          
             <p>{blog.date}</p>
             <p>{blog.duration} min read</p>
           </div>
